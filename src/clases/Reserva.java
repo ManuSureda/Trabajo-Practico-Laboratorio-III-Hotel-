e58b0c567eaea9ea9fd0cbec06bf7c7ac4d83adb
DMasudd <DMasudd@users.noreply.github.com>
2018-06-27 17:40:51 -0300

    mod1.0 reserva

--- src/clases/Reserva.java
+++ src/clases/Reserva.java
@@ -1,27 +1,49 @@
-package app;
+package clases;
 
 import java.util.ArrayList;
 
 public class Reserva {
 	
 	private double costo;
-	private String dni;
+	private int id;
 	private boolean reservaUsada;
 	private Fechas fechasOcupadas;
 	private ArrayList<Habitacion> habitacionesRequeridas;
+	private static int contadorId;
 
-	public Reserva(double costoParam, String dniParam, ArrayList<Habitacion> habitacionesParam, Fechas fechasParam) {
+	public Reserva(double costoParam, ArrayList<Habitacion> habitacionesParam, Fechas fechasParam) {
 		costo = costoParam;
-		dni = dniParam;
+		contadorId++;
+		id = contadorId;
 		reservaUsada = false;
 		habitacionesRequeridas = habitacionesParam;
 		fechasOcupadas = fechasParam;
 	}
 	
+	public Reserva() {
+		contadorId = 1;
+	}
+	
+	public double getCosto() {
+		return costo;
+	}
+	public int getId() {
+		return id;
+	}
+	public Fechas getFechasOcupadas() {
+		return fechasOcupadas;
+	}
+	public ArrayList<Habitacion> getHabitacionesRequeridas() {
+		return habitacionesRequeridas;
+	}
+	
+	public void setReservaUsada(boolean reservaUsadaParam) {
+		reservaUsada = reservaUsadaParam;
+	}
 	
 	public void mostrarReserva() {
-		System.out.println("ID Reserva: "+dni);
-		System.out.println("Fechas ocupadas: "+fechasOcupadas.getFechaIn()+ " al "+fechasOcupadas.getFechaOut());
+		System.out.println("ID Reserva: "+id);
+		System.out.println("Fechas ocupadas: "+fechasOcupadas.getFechaInString()+ " al "+fechasOcupadas.getFechaOutString());
 		System.out.println("Habitaciones requeridas: ");
 			for(Habitacion aux:habitacionesRequeridas) {
 				System.out.println(aux);
@@ -33,9 +55,7 @@ public class Reserva {
 		}
 	}
 	
-	public void setReservaUsada(boolean reservaUsadaParam) {
-		reservaUsada = reservaUsadaParam;
-	}
+
 
 	
 }
