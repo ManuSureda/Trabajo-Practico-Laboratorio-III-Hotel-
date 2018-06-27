e6569b23904af17310625e3509cfbdb86752c3ad
maxu99 <33207486+maxu99@users.noreply.github.com>
2018-06-27 19:32:47 -0300

    Cliente

--- src/clases/Cliente.java
+++ src/clases/Cliente.java
@@ -1,11 +1,23 @@
 package clases;
 
+import java.util.ArrayList;
+
 public class Cliente extends Usuario {
 
+	ArrayList <Integer>numeroReservas=new ArrayList();
+	
 	public Cliente(String nombreCompleto, String dni, String telefono, String domicilio, String nombreUsuarioParam,
 			String passwordParam) {
 		super(nombreCompleto, dni, telefono, domicilio, nombreUsuarioParam, passwordParam);
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	public void consultarReservas()
+	{
+		for(Integer a:numeroReservas)
+		{
+			BaseDeDatos.getReservas().get(a).mostrarReserva();
+		}
+	}
+	
 }
